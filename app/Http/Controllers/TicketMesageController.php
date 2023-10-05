8271a7f2f02fa25a00bffb2f2313451a4c8f7bf4
Claudio Perroni <c.perroni@ifortech.com>
2023-10-05 08:14:19 +0200

    nuove tabelle

---
+++ app/Http/Controllers/TicketMesageController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TicketMesage;
+use Illuminate\Http\Request;
+
+class TicketMesageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TicketMesage $ticketMesage)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TicketMesage $ticketMesage)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, TicketMesage $ticketMesage)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TicketMesage $ticketMesage)
+    {
+        //
+    }
+}
